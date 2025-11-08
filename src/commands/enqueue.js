300525ae89fd4e734a8e1ceed1076f523e182baa
5ar1m <2022umt1495@gmail.com>
2025-11-09 05:09:57 +0530

    add enqueue feature

--- src/commands/enqueue.js
+++ src/commands/enqueue.js
@@ -10,11 +10,11 @@ enqueue
 .argument('<job-data>', 'the json string containing job details')
 .action(async (jobData) => {
     // logic for enqueueing the job to queue
+    const job = JSON.parse(jobData);
     await insertNew({
-        title: jobData['title'],
-        command: jobData['command'],
-        createdAt: jobData['createdAt'] || 'CURRENT_TIMESTAMP',
-
+        title: job['title'],
+        command: job['command'],
+        createdAt: job['createdAt'] ? `'${job['createdAt']}'` : 'CURRENT_TIMESTAMP' // if inserting the job first time then current time stamp else other option for jobs to be retried
     }, 'job_queue');
 });
 
--- src/queue/insertNew.js
+++ src/queue/insertNew.js
@@ -3,14 +3,14 @@ const asyncRun = require('../db/asyncRun.js');
 async function insertNew(jobData, table) {
     let query = ``;
     if (table == 'job_queue') {
-        query = `INSERT INTO job_queue(command, created_at, updated_at)
-                VALUES(${jobData['command']}, ${jobData['createdAt']}, CURRENT_TIMESTAMP)`;
+        query = `INSERT INTO job_queue (title, command, created_at)
+                VALUES ('${jobData['title']}', '${jobData['command']}', ${jobData['createdAt']})`;
     } else if (table == 'dead_letter_queue') {
-        query = `INSERT INTO dead_letter_queue(id, command, max_retries, created_at, updated_at)
-                VALUES(${jobData['id']}, ${jobData['command']}, ${jobData['maxRetries']}, ${jobData['createdAt']}, CURRENT_TIMESTAMP)`;
+        query = `INSERT INTO dead_letter_queue (id, title, command, max_retries, created_at, updated_at)
+                VALUES ('${jobData['id']}', '${jobData['title']}', '${jobData['command']}', ${jobData['maxRetries']}, '${jobData['createdAt']}', CURRENT_TIMESTAMP)`;
     } else {
-        query = `INSERT INTO archive(id, command, attempts, max_retries, created_at, updated_at)
-                VALUES(${jobData['id']}, ${jobData['command']}, ${jobData['attempts']}, ${jobData['maxRetries']}, ${jobData['createdAt']}, CURRENT_TIMESTAMP)`;   
+        query = `INSERT INTO archive (id, title, command, attempts, max_retries, created_at, updated_at)
+                VALUES (${jobData['id']}, '${jobData['title']}', '${jobData['command']}', ${jobData['attempts']}, ${jobData['maxRetries']}, '${jobData['createdAt']}', CURRENT_TIMESTAMP)`;   
     }
 
     try {
